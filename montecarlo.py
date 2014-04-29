78aff570114c202b19d3ff35dcb97ca6f39c3658
Lettergram <Lettergramm@gmail.com>
2014-04-28 22:24:12 -0500

    Added second comparison function

--- montecarlo.py
+++ montecarlo.py
@@ -1,16 +1,24 @@
 import numpy as np
 
-def func(x, y):
+def func1(x, y):
     return x*x + x*y + y*y
 
-def monte(n, ymin, ymax, xmin, xmax):
+def func2(y, x):
+    return x**3 + y**3 + y*x**2 + y**2 + 2
+
+def monte(func, n, ymin, ymax, xmin, xmax):
 
     sum = 0
-    x = np.random.uniform(ymin, ymax, n)
-    y = np.random.uniform(xmin, xmax, n)
-    for i in range(n):
-        sum += func(x[i], y[i])
-        print(i, (2 * sum)/(i+1))
-    return (2 * sum) / n
+    x = np.random.uniform(xmin, xmax, n)
+    y = np.random.uniform(ymin, ymax, n)
 
-print monte(10000, 0, 1, 2, 4);
+    for i in range(n):
+        sum += 2 * func(x[i], y[i])
+        print(i, sum / (i+1))
+
+    return sum / n
+
+total1 = monte(func1, 30000, 0, 1, 2, 4)
+total2 = monte(func2, 30000, 1, 3, 1, 2)
+
+print(total1, total2)
